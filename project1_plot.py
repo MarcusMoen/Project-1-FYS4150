c3a345dc24d10f7b80dedb64887008f837bb8b42
MarcusMoen <45385797+MarcusMoen@users.noreply.github.com>
2021-09-11 16:06:43 +0200

    Update project1_plot.py

--- project1_plot.py
+++ project1_plot.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import matplotlib
+import math
 
 matplotlib.rcParams.update({'font.size':14})
 
@@ -20,3 +21,20 @@ plt.xlabel('x')
 plt.ylabel('u(x)')
 plt.title('Poisson equation')
 plt.savefig('poisson1.pdf', dpi=900)
+
+
+#Task 8a
+def absolute_log_error(u, v):
+    abs_error = []
+    for i in range(len(u)):
+        abs_error.append(math.log10(abs(u[i]-v[i])))
+    return abs_error
+
+#Task 8b
+def relative_log_error(u, v):
+    abs_error = []
+    for i in range(len(u)):
+        abs_error.append(math.log10(abs((u[i]-v[i])/u[i])))
+    return abs_error
+    
+    
