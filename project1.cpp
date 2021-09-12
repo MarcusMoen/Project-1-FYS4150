29128d6fb5c4c56ba6ecbe9b4f3ccef4e7ebc73d
ThorsteinSkaug <thorstein.skaug@fys.uio.no>
2021-09-12 11:56:17 +0200

    Gjorde noen endringer på plottene, la også til et plot for n=1000

--- project1.cpp
+++ project1.cpp
@@ -11,7 +11,7 @@ int main()
   // Print a message to screen
 
   // Problem 2:
-  int n = 100;
+  int n = 1000;
   double k = 0.0;
   double x[n];
   double u[n];
--- project1_plot.py
+++ project1_plot.py
@@ -10,18 +10,48 @@ df = pd.read_csv('data.txt', sep=' ', header=None)
 x = df[0].to_numpy()
 u = df[1].to_numpy()
 
+df = pd.read_csv('approx_with_10_xes.txt', sep=' ', header=None)
+x1 = df[0].to_numpy()
+v1 = df[1].to_numpy()
+
 df = pd.read_csv('approx_with_100_xes.txt', sep=' ', header=None)
 x2 = df[0].to_numpy()
-v = df[1].to_numpy()
+v2 = df[1].to_numpy()
 
-plt.plot(x,u)
-plt.plot(x2,v)
-plt.grid()
-plt.xlabel('x')
-plt.ylabel('u(x)')
-plt.title('Poisson equation')
-plt.legend(['Real solution', 'Approximated solution with n points'], bbox_to_anchor = (1.05, 0.6))
-plt.savefig('poisson1.pdf', dpi=900)
+df = pd.read_csv('approx_with_1000_xes.txt', sep=' ', header=None)
+x3 = df[0].to_numpy()
+v3 = df[1].to_numpy()
+
+fig, ax = plt.subplots(3,1, figsize=(8,12))
+
+plt.suptitle('Poisson equation', fontsize=18)
+plt.subplots_adjust(top=0.95)
+
+ax[0].plot(x,u)
+ax[0].plot(x1,v1)
+ax[0].grid()
+ax[0].set_xlabel('x')
+ax[0].set_ylabel('u(x)')
+ax[0].legend(['Real solution', 'Approximated solution \n with 10 points'] ,bbox_to_anchor = (0.57, 1), fontsize=12)
+
+ax[1].plot(x,u)
+ax[1].plot(x2,v2)
+ax[1].grid()
+ax[1].set_xlabel('x')
+ax[1].set_ylabel('u(x)')
+ax[1].legend(['Real solution', 'Approximated solution \n with 100 points'] ,bbox_to_anchor = (0.57, 1), fontsize=12)
+
+ax[2].plot(x,u)
+ax[2].plot(x3,v3)
+ax[2].grid()
+ax[2].set_xlabel('x')
+ax[2].set_ylabel('u(x)')
+ax[2].legend(['Real solution', 'Approximated solution \n with 1000 points'] ,bbox_to_anchor = (0.57, 1), fontsize=12)
+
+plt.savefig('poisson.pdf', dpi=900)
+plt.show()
+
+
 
 
 #Task 8a
@@ -37,5 +67,3 @@ def relative_log_error(u, v):
     for i in range(len(u)):
         abs_error.append(math.log10(abs((u[i]-v[i])/u[i])))
     return abs_error
-    
-    
