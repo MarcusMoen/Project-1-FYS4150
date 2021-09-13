bf97b5e536944a8e5d1b56efdefcc295fe6a684c
MarcusMoen <45385797+MarcusMoen@users.noreply.github.com>
2021-09-13 09:07:04 +0200

    Update project1.cpp
    Added comments

--- project1.cpp
+++ project1.cpp
@@ -8,18 +8,20 @@ using std::vector;
 
 int main()
 {
-  // Print a message to screen
 
   // Problem 2:
-  int n = 10000;
+  int n = 10; //The number of steps
   double k = 0.0;
-  double x[n];
-  double u[n];
+  double step = 1./n; //Step length
+  double x[n]; //Storing the x-values
+  double u[n]; //Storing the true values of u in the x-points
   std::ofstream myfile;
   myfile.open("data.txt");
-  for(int i = 0; i <= n; i++){
+  
+  //Add all the values to the vectors x and u.
+  for(int i = 0; i <= n; i++){ 
     x[i] = k;
-    k += 1./n;
+    k += step;
     u[i] = 1-(1-exp(-10))*x[i]-exp(-10*x[i]);
     std::cout.precision(4);
     myfile << std::scientific << x[i] << " " << std::scientific << u[i] << "\n";
@@ -28,58 +30,54 @@ int main()
 
 
   //Problem 7
-  double a[n-2];
-  double b[n-2];
-  double c[n-2];
-  double g[n-2];
+  double a[n-2]; //Stores the sub-diagonal values
+  double b[n-2]; //Stores the main-diagonal values
+  double c[n-2]; //Stores the super-diagonal values
+  double g[n-2]; //Stores the g values for problem 7a
+  double g_special[n-2]; //Stores the g values for problem 9b
+  
+  //Add all the necessary values to the arrays above
   for(int i=0; i<=n-3; i++){
     g[i] = 100*exp(-10*x[i+1])*pow(1./n,2);
+    g_special[i] = 100*exp(-10*x[i+1])*pow(1./n,2);
     a[i] = -1;
     b[i] = 2;
     c[i] = -1;
-    //std::cout << i << ' ' << g[i] << ' ' << x[i] << "\n";
   }
 
+  
   double b_update[n-2];
   b_update[0] = b[0];
+  //Update the main-diagonal values and the g-values
   for(int i=1; i<=n-3; i++){
       b_update[i] = b[i] - (a[i]/b_update[i-1])*c[i];
       g[i] = g[i]-(a[i]/b_update[i-1])*g[i-1];
-      //std::cout << 'g' << ' ' << mat[i][i-1] << ' ' << g[i] << ' ' << "Dette er g" << ' ' << g[i] << "\n";
   }
 
 
-  double v[n-2];
+  double v[n-2]; //Stores the approx solution
   v[n-3] = g[n-3]/b_update[n-3];
   myfile.open("approx_with_" + std::to_string(n) + "_xes.txt");
   myfile << std::scientific << x[n-1] << " " << std::scientific << 0 << "\n";
   myfile << std::scientific << x[n-2] << " " << std::scientific << v[n-3] << "\n";
+  
+  //Find all the approx solutions
   for(int i=n-4; i>=0; i--){
     v[i] = (g[i] - c[i]*v[i+1])/b_update[i];
-    //std::cout << "b tilda" << ' ' << A_new[i][i] << '\n';
-    std::cout << "v_i+1" << ' ' << v[i+1] << '\n';
-    std::cout << "g tilda" << ' ' << g[i] << '\n';
-
-
     std::cout.precision(4);
     myfile << std::scientific << x[i+1] << " " << std::scientific << v[i] << "\n";
-    std::cout << i << ' ' << v[i] << "\n";
   }
   myfile << std::scientific << x[0] << " " << std::scientific << 0 << "\n";
-
-  std::cout << v[2];
-  std::cout << '\n';
-  std::cout << u[3];
-  std::cout << '\n';
-
+  myfile.close();
 
   //Problem 9b
+  //Update the g-values
   for(int i=1; i<=n-3; i++){
     double b_tilde = 1.0*i/(i+1);
     g_special[i] = g_special[i]+b_tilde*g_special[i-1];
   }
 
-
+  //Does the same as problem 7a but with a slithly different algorithm
   double v_special[n-2];
   double multi = 1.0*(n-2)/(n-1);
   v_special[n-3] = multi * g_special[n-3];
@@ -96,6 +94,7 @@ int main()
   }
   myfile << std::scientific << x[0] << " " << std::scientific << 0 << "\n";
   myfile.close();
+
 
 
 
